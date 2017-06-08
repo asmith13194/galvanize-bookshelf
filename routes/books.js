29283e9d2b1a3e6b0bb9e2aaecd1eee888fff640
Adam Smith <asmith13194@gmail.com>
2017-06-07 20:30:09 -0600

    better err handler for books

--- routes/books.js
+++ routes/books.js
@@ -12,49 +12,53 @@ const router = express.Router();
 
 // YOUR CODE HERE
 
-router.get('/books',(req,res,next)=>{
+router.get('/books', (req, res, next) => {
   knex('books')
-    .select('id','genre','title','author','description','cover_url as coverUrl','created_at as createdAt','updated_at as updatedAt').orderBy('title')
-    .then((result)=>{
-    res.send(result);
-  })
-})
-
-router.get('/books/:id',(req,res,next)=>{
-  knex('books')
-    .count('id')
-    .then(total=>{
-      if (req.params.id<1||req.params.id>total[0].count){
-        return res.sendStatus(404)
-      }
-      knex('books')
-        .select('id','genre','title','author','description','cover_url as coverUrl','created_at as createdAt','updated_at as updatedAt').where('id','=',req.params.id)
-        .then((result)=>{
-
-        res.send(result);
-      })
+    .select('id', 'genre', 'title', 'author', 'description', 'cover_url as coverUrl', 'created_at as createdAt', 'updated_at as updatedAt').orderBy('title')
+    .then((result) => {
+      res.send(result);
     })
 })
 
-router.post('/books',(req,res,next)=>{
+router.get('/books/:id', (req, res, next) => {
+  if (parseInt(req.params.id) !== typeof(1)) {
+    return res.sendStatus(404)
+  }
+  knex('books')
+    .select('id')
+    .where('id', '=', req.params.id)
+    .then(total => {
+      if (total.length === 0) {
+        return res.sendStatus(404)
+      }
+      knex('books')
+        .select('id', 'genre', 'title', 'author', 'description', 'cover_url as coverUrl', 'created_at as createdAt', 'updated_at as updatedAt').where('id', '=', req.params.id)
+        .then((result) => {
+
+          res.send(result);
+        })
+    })
+})
+
+router.post('/books', (req, res, next) => {
   // console.log(req.body);
-  if(req.body.title===undefined){
+  if (req.body.title === undefined) {
     res.status(400);
     res.setHeader('content-type', 'text/plain');
     return res.send('Title must not be blank');
-  }else if(req.body.author===undefined){
+  } else if (req.body.author === undefined) {
     res.status(400);
     res.setHeader('content-type', 'text/plain');
     return res.send('Author must not be blank');
-  }else if(req.body.genre===undefined){
+  } else if (req.body.genre === undefined) {
     res.status(400);
     res.setHeader('content-type', 'text/plain');
     return res.send('Genre must not be blank');
-  }else if(req.body.description===undefined){
+  } else if (req.body.description === undefined) {
     res.status(400);
     res.setHeader('content-type', 'text/plain');
     return res.send('Description must not be blank');
-  }else if(req.body.coverUrl===undefined){
+  } else if (req.body.coverUrl === undefined) {
     res.status(400);
     res.setHeader('content-type', 'text/plain');
     return res.send('Cover URL must not be blank');
@@ -62,46 +66,54 @@ router.post('/books',(req,res,next)=>{
   knex('books')
     .returning('*')
     .insert(humps.decamelizeKeys(req.body))
-    .then(result=>{
+    .then(result => {
       res.send(humps.camelizeKeys(result[0]))
     })
 })
 
-router.patch('/books/:id',(req,res,next)=>{
+router.patch('/books/:id', (req, res, next) => {
+  if (parseInt(req.params.id) !== typeof(1)) {
+    return res.sendStatus(404)
+  }
   knex('books')
-    .count('id')
-    .then(total=>{
-      if (req.params.id<1||req.params.id>total[0].count){
+    .select('id')
+    .where('id', '=', req.params.id)
+    .then(total => {
+      if (total.length === 0) {
         return res.sendStatus(404)
       }
-      req.body['id']=req.params.id
+      req.body['id'] = req.params.id
       knex('books')
-        .where('id','=',req.params.id)
+        .where('id', '=', req.params.id)
         .update(req.body)
-        res.send(req.body)
+      res.send(req.body)
     })
 
 
 })
 
-router.delete('/books/:id',(req,res,next)=>{
+router.delete('/books/:id', (req, res, next) => {
+  if (parseInt(req.params.id) !== typeof(1)) {
+    return res.sendStatus(404)
+  }
   knex('books')
-    .count('id')
-    .then(total=>{
-      if (req.params.id<1||req.params.id>total[0].count){
+    .select('id')
+    .where('id', '=', req.params.id)
+    .then(total => {
+      if (total.length === 0) {
         return res.sendStatus(404)
       }
       knex('books')
         .returning('*')
-        .where('id','=',req.params.id)
+        .where('id', '=', req.params.id)
         .del()
-        .then(result=>{
+        .then(result => {
           delete result[0].id
           delete result[0].created_at
           delete result[0].updated_at
           res.send(humps.camelizeKeys(result[0]))
+        })
     })
-  })
 })
 
 module.exports = router;
